b26cfd81b227e21571c990c772a37128bda0b516
Alex Nielsen <alex@bigal.org>
2024-06-03 09:43:22 -0400

    start at problems of token based auth

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -18,12 +19,22 @@ blogsRouter.get('/:id', async (request, response, next) => {
 
 })
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
+
 blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
     console.log("request.body", body.userId)
-
-    const user = await User.findById(body.userId)
-    console.log(user)
 
     const blog = new Blog({
         title: body.title,
